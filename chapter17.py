822da4570af57e188624e0f509a255eacd03b65c
Sean <sean.mahoney743@gmail.com>
2019-09-21 16:10:04 -0400

    added largest rectangle problem

--- chapter17.py
+++ chapter17.py
@@ -8,3 +8,16 @@ def max_water(heights):
         if heights[right] < heights[left]:
             right -= 1
         else: left -= 1
+
+def calculate_largest_rectangle(heights):
+    max_area, pillars = 0, []
+
+    for i, height in enumerate(heights):
+        while height < pillars[-1] and pillars:
+            pillar_height = heights[pillars.pop()]
+            width = i if not pillars else i - pillars[-1] - 1
+            max_area = max(max_area, pillar_height * width)
+        
+        pillars.append(i)
+
+
